dd3dfe1273a2014b755fa4574812169352efc779
Enese Serobe <rodriguezserobe@gmail.com>
2026-05-26 15:25:25 +0200

    added navbar and clerk auth

--- umadeit/src/App.jsx
+++ umadeit/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ApplyJobPage from "./pages/ApplyJobPage";
+import ApplicationPage from "./pages/ApplicationPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/apply-job/:id" element={<ApplyJobPage />} />
+        <Route path="/application" element={<ApplicationPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ umadeit/src/pages/ApplyJobPage.jsx
@@ -0,0 +1,5 @@
+const ApplyJobPage = () => {
+  return <div>ApplyJobPage</div>;
+};
+
+export default ApplyJobPage;
